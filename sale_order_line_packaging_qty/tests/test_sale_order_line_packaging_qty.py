83e71f669d4b3c44013dea82e8fbdf48a228d904
Hai Lang <hailn@trobz.com>
2021-07-10 13:33:52 +0700

    [IMP] sale_order_line_packaging_qty: increase coverage

--- sale_order_line_packaging_qty/tests/test_sale_order_line_packaging_qty.py
+++ sale_order_line_packaging_qty/tests/test_sale_order_line_packaging_qty.py
@@ -16,6 +16,20 @@ class TestSaleOrderLinePackagingQty(SavepointCase):
         )
         cls.product_no_pckg = cls.env.ref("product.product_product_10")
 
+    def _check_sequence(self):
+        IrSequence = self.env['ir.sequence']
+        SaleOrder = self.env['sale.order']
+        sos = SaleOrder.search([('name', '=like', 'SQ%')])
+        names = sos.mapped('name')
+        print('names', names)
+        in_loop = True
+        while in_loop:
+            sq_seq = IrSequence.search([('code', '=', 'sale.quotation')])[0]
+            print('number_next_actual', sq_seq.number_next_actual)
+            in_loop = 'SQ{:0>3}'.format(sq_seq.number_next_actual) in names
+            if in_loop:
+                IrSequence.next_by_code('sale.quotation')
+
     def test_product_packaging_qty(self):
         order = self.env["sale.order"].create({"partner_id": self.partner.id})
         order_line = self.env["sale.order.line"].create(
@@ -39,20 +53,26 @@ class TestSaleOrderLinePackagingQty(SavepointCase):
         order_line._onchange_product_packaging_qty()
         self.assertEqual(order_line.product_uom_qty, 15.0)
 
+        order_line.write({"product_uom_qty": 9.0})
+        self.assertEqual(order_line.product_packaging_qty, 0.0)
+        order_line.write({"product_uom_qty": 15.0})
+
         dozen = self.env.ref("uom.product_uom_dozen")
         order_line.product_uom = dozen
         order_line._compute_product_packaging_qty()
         self.assertEqual(order_line.product_uom_qty, 180.0)
+        self.assertEqual(order_line.product_uom, self.product.uom_id)
 
         self.packaging.qty = 0
         with self.assertRaises(UserError):
             order_line.write({"product_packaging_qty": 0.0})
+        self.packaging.qty = 5
 
         order_line.product_packaging = False
         order_line._onchange_product_packaging()
         self.assertEqual(order_line.product_packaging_qty, 0.0)
 
-    def test_product_packaging_qty_wo_packaging(self):
+    # def test_product_packaging_qty_wo_packaging(self):
         order = self.env["sale.order"].create({"partner_id": self.partner.id})
         order_line = self.env["sale.order.line"].create(
             {
@@ -67,3 +87,24 @@ class TestSaleOrderLinePackagingQty(SavepointCase):
         self.assertEqual(order_line.product_packaging_qty, 0.0)
         with self.assertRaises(UserError):
             order_line.write({"product_packaging_qty": 3.0})
+        # self._check_sequence()
+
+    # def test_product_packaging_qty_from_external(self):
+        """The previous ones have product_uom_qty of 3, which is less than and
+        not divisible by packaging qty of 5. This test is to increase coverage
+        for the case that product_uom_qty of 15 is divisible by 5.
+        """
+        order = self.env["sale.order"].create({"partner_id": self.partner.id})
+        order_line = self.env["sale.order.line"].create(
+            {
+                "order_id": order.id,
+                "product_id": self.product.id,
+                "product_uom": self.product.uom_id.id,
+                "product_uom_qty": 15.0,
+                "product_packaging": self.packaging.id,
+            }
+        )
+        print('order_line.product_packaging.qty', order_line.product_packaging.qty)
+        order_line._onchange_product_packaging()
+        self.assertEqual(order_line.product_packaging_qty, 3.0)
+        self._check_sequence()
